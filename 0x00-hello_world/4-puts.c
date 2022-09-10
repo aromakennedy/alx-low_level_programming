ef92f2078ebf6a221adfd3e524643530a652ab41
aromakennedy <kennedyaroma7@gmail.com>
2022-09-10 15:25:08 -0700

    task f4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-/** 
- main - Entry point
- Description: use of 'mains function'
- Return: Always 0 (Success)
+/**
+ *
+ * main - Entry point
+ *Description: use of 'mains function'
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");	
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 
 }
