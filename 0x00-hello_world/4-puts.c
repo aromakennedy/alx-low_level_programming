669c1f922121367d22a7d7ae859fc3996c31e848
aromakennedy <kennedyaroma7@gmail.com>
2022-09-10 13:42:53 -0700

    number four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,12 @@
 
 /**
  * main-print the string in the put function
- *
- * Description:using the main function
- * this program prints 'programming is like building a multilingual puzzle'
- * Return:0
+ * description: using the main function
+ * this program prints 'programming is like building a multilingual puzzle
+ * return :0
  */
 int main(void)
 {
-
-puts("\"programming is like building a multilingual puzzle");
-return(0);
-}
+	put("\"programming is like building a multilingual puzzle");
+	return (0);
+}	
