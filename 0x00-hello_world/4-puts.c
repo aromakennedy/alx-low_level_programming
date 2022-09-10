0f76c910d0b60750efa19ef791d1fcf0ca700085
aromakennedy <kennedyaroma7@gmail.com>
2022-09-10 14:53:28 -0700

    TASK 4-PUTS.C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,17 +4,16 @@
 
 /**
  *
- * main - Entry point
- * description : using the main function
- * this program prints 'progamming is like bilding a multilingual pu
- *  Return: Always 0 (Success)
+ *   main - Entry point
+ *
+ *   Description: use of 'mains function'
+ *
+ *    Return: Always 0 (Success)
+ *
  */
-
-int main(void) 
- 
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle")	
 return (0);
 
 }
-
