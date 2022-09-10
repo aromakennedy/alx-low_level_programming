91f8a958d325b9db42368b909e7bea93b7fb3f55
aromakennedy <kennedyaroma7@gmail.com>
2022-09-10 15:35:10 -0700

    Hllo,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- *
- * main - Entry point
- *Description: use of 'mains function'
- *Return: Always 0 (Success)
- */
+*
+* main - Entry point
+* Description: use of 'mains function'
+* Return: Always 0 (success)
+*
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
