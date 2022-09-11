b83146c96bfff52cd8d54f7cf6602c74034ece7d
aromakennedy <kennedyaroma7@gmail.com>
2022-09-11 03:06:32 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,38 +4,29 @@
  * main - entry point
  * Return: alway returns 0
 */
+
 int main(void)
 
 {
 
-	int i;
-
-	int j;
-
-	int k;
-
-
+int i;
+int j;
+int k;
 
 	for (i = 0; i <= 9; i++)
-
-	{
+{
 
 		for (j = 0; j <= 9; j++)
-
 		{
 
 			for (k = 0; k <= 9; k++)
-
 			{
 
 				if (k > j && j > i)
-
 				{
 
 					putchar(i + '0');
-
 					putchar(j + '0');
-
 					putchar(k + '0');
 
 					if (i != 7 || j != 8 || k != 9)
@@ -43,17 +34,12 @@ int main(void)
 					{
 
 						putchar(',');
-
 						putchar(' ');
 
 					}
-
 				}
-
 			}
-
 		}
-
 	}
 
 	putchar('\n');
