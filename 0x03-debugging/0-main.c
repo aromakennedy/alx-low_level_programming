5e92a8023d320dbed2c8ae680059d80f74a88843
aromakennedy <kennedyaroma7@gmail.com>
2022-09-16 18:22:36 -0700

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,7 +6,6 @@
 * Return: 0
 */
 
-
 int main(void)
 
 {
@@ -14,11 +13,9 @@ int main(void)
 int i;
 
 
+  i = 98 % 2;
 
-i = 0;
-
-positive_or_negative(i);
-
+    positive_or_negative(i);
 
 
 return (0);
