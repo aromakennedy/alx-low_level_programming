69f0f68462e063f50c34bed1f3ff3dd176a95722
aromakennedy <kennedyaroma7@gmail.com>
2022-09-18 10:07:37 -0700

    2. 0 grater than 972

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -10,7 +10,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void print_remaining_days(int, int, int);
+void print_remaining_days(int a, int b, int c);
 int convert_day(int, int);
 
 
