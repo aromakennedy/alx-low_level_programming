7cc44f4b585f97d8ba77a34521315dd8e063d1e0
aromakennedy <kennedyaroma7@gmail.com>
2022-09-22 12:57:31 -0700

    5. luk up always

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,39 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * cap_string - capitalizes all words of a string
- * @s: input string.
- * Return: the pointer to dest.
+ * string_toupper - changes all lowercase letters to uppercase
+ * @s: string to change
+ *
+ * Return: address of s
  */
-
-char *cap_string(char *s)
-
+char *string_toupper(char *s)
 {
-	int count = 0, i;
+	int i = 0;
 
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
-	{
-		for (i = 0; i < 13; i++)
+	while (*(s + i))
 
 		{
-			if (*(s + count) == sep_words[i])
-
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-			break;
-			}
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+			i++;
 		}
-	}
-	count++;
-	
-	return (s);
+		return (s);
 }
