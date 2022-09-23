afbb9ba410baca38ef3374acbba256e2403e943e
aromakennedy <kennedyaroma7@gmail.com>
2022-09-22 19:56:30 -0700

    task nine numbers have life

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,47 +1,32 @@
 #include "main.h"
-
-
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
- */
-
+* print_number - prints an integer
+*@n:integer to be printed
+*
+*/
 void print_number(int n)
 
 {
-	unsigned int m, d, count;
+	unsigned int num = n;
 
 	if (n < 0)
 
 	{
-		putchar(45);
 
-		m = n * -1;
-	}
+		_putchar('-');
 
-	else
-
-	{
-		m = n;
+		num = -num;
 
 	}
 
-	d = m;
 
-	count = 1;
 
-	while (d > 9)
+	if ((num / 10) > 0)
 
-	{
-		d /= 10;
+		print_number(num / 10);
 
-		count *= 10;
-	}
 
-	for (; count >= 1; count /= 10)
-	{
 
-		_putchar(((m / count) % 10) + 48);
-	}
+	_putchar((num % 10) + '0');
+
 }
